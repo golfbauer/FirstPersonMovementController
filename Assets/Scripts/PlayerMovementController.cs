427cf93276aee5e21228cf11daddb330be9995af
Henri Truetsch <87329520+golfbauer@users.noreply.github.com>
2022-10-27 16:52:48 +0200

    Clean up Files, Make attributes accessable via Unity editor and Scripts during Runtime (#4)
    * feat: make attributes accessable via Unity editor during runtime
    
    * fix: remove bool

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -5,47 +5,74 @@ using UnityEngine;
 
 public class PlayerMovementController : MonoBehaviour
 {
+    [Header("Debugging")]
+    [SerializeField] private bool configDuringRuntime = false;
+
     [Header("Kinematic Character Controller")]
-    [SerializeField] private float slopeLimit;
-    [SerializeField] private Vector3 center;
-    [SerializeField] private float height;
-    [SerializeField] private float radius;
-    [SerializeField] private float anglePower;
-    [SerializeField] private float maxBounces;
+    [SerializeField] [OnChangedCall("OnVariableChange")] private float slopeLimit;
+    [SerializeField] [OnChangedCall("OnVariableChange")] private Vector3 center;
+    [SerializeField] [OnChangedCall("OnVariableChange")] private float height;
+    [SerializeField] [OnChangedCall("OnVariableChange")] private float radius;
+    [SerializeField] [OnChangedCall("OnVariableChange")] private float anglePower;
+    [SerializeField] [OnChangedCall("OnVariableChange")] private float maxBounces;
 
     [Header("Player Components")]
-    [SerializeField] private Transform playerTransform;
-    [SerializeField] private GameObject playerCamera;
+    [SerializeField] [OnChangedCall("OnVariableChange")] private Transform playerTransform;
+    [SerializeField] [OnChangedCall("OnVariableChange")] private GameObject playerCamera;
 
     [Header("Mouse Configurations")]
-    [SerializeField] private float mouseSensivity = 100f;
+    [SerializeField] [OnChangedCall("OnVariableChange")] private float mouseSensivity = 100f;
 
     [Header("Movement Configurations")]
-    [SerializeField] private float moveSpeed = 20f;
-    [SerializeField] private float sprintSpeed = 40f;
+    [SerializeField] [OnChangedCall("OnVariableChange")] private float moveSpeed = 20f;
+    [SerializeField] [OnChangedCall("OnVariableChange")] private float sprintSpeed = 40f;
 
     [Header("Jumping Configurations")]
-    [SerializeField] private float jumpForce = 1.0f;
-    [SerializeField] private int countAllowedJumps = 1;
+    [SerializeField] [OnChangedCall("OnVariableChange")] private float jumpForce = 1.0f;
+    [SerializeField] [OnChangedCall("OnVariableChange")] private int countAllowedJumps = 1;
 
     [Header("Crouch Configurations")]
-    [SerializeField] private float crouchHeight;
-    [SerializeField] private float standingHeight;
-    [SerializeField] private float timeToCrouch;
-    [SerializeField] private Vector3 crouchingCenter;
-    [SerializeField] private Vector3 standingCenter;
+    [SerializeField] [OnChangedCall("OnVariableChange")] private float crouchHeight;
+    [SerializeField] [OnChangedCall("OnVariableChange")] private float standingHeight;
+    [SerializeField] [OnChangedCall("OnVariableChange")] private float timeToCrouch;
+    [SerializeField] [OnChangedCall("OnVariableChange")] private Vector3 crouchingCenter;
+    [SerializeField] [OnChangedCall("OnVariableChange")] private Vector3 standingCenter;
 
     [Header("Environmental Configurations")]
-    [SerializeField] private Vector3 gravity = new Vector3(0, -9.81f, 0);
+    [SerializeField] [OnChangedCall("OnVariableChange")] private Vector3 gravity = new Vector3(0, -9.81f, 0);
 
     [Header("Key Binding Configurations")]
-    [SerializeField] private KeyCode sprintKey = KeyCode.LeftShift;
-    [SerializeField] private KeyCode jumpKey = KeyCode.Space;
-    [SerializeField] private KeyCode crouchKey = KeyCode.LeftControl;
+    [SerializeField] [OnChangedCall("OnVariableChange")] private KeyCode sprintKey = KeyCode.LeftShift;
+    [SerializeField] [OnChangedCall("OnVariableChange")] private KeyCode jumpKey = KeyCode.Space;
+    [SerializeField] [OnChangedCall("OnVariableChange")] private KeyCode crouchKey = KeyCode.LeftControl;
 
     private PlayerCameraLook mouseLookCamera;
     private PlayerMovement playerMovement;
     private KinematicCharacterController controller;
+
+    public PlayerCameraLook MouseLookCamera
+    {
+        get
+        {
+            return mouseLookCamera;
+        }
+    }
+
+    public PlayerMovement PlayerMovement
+    {
+        get
+        {
+            return playerMovement;
+        }
+    }
+
+    public KinematicCharacterController Controller
+    {
+        get
+        {
+            return controller;
+        }
+    }
 
 
     private void Awake()
@@ -79,5 +106,44 @@ public class PlayerMovementController : MonoBehaviour
             countAllowedJumps
             );
 
+    }
+
+    public void OnVariableChange()
+    {
+        if (configDuringRuntime)
+        {
+            if(controller != null)
+            {
+                controller.SlopeLimit = slopeLimit;
+                controller.Center = center;
+                controller.Height = height;
+                controller.Radius = radius;
+                controller.AnglePower = anglePower;
+                controller.MaxBounces = maxBounces;
+            }
+
+            if (mouseLookCamera != null)
+            {
+                mouseLookCamera.PlayerTransform = playerTransform;
+                mouseLookCamera.MouseSensitivity = mouseSensivity;
+            }
+
+            if (playerMovement != null)
+            {
+                playerMovement.MoveSpeed = moveSpeed;
+                playerMovement.SprintSpeed = sprintSpeed;
+                playerMovement.SprintKey = sprintKey;
+                playerMovement.JumpKey = jumpKey;
+                playerMovement.CrouchHeight = crouchHeight;
+                playerMovement.StandingHeight = standingHeight;
+                playerMovement.TimeToCrouch = timeToCrouch;
+                playerMovement.CrouchingCenter = crouchingCenter;
+                playerMovement.StandingCenter = standingCenter;
+                playerMovement.CrouchKey = crouchKey;
+                playerMovement.Gravity = gravity;
+                playerMovement.JumpForce = jumpForce;
+                playerMovement.CountAllowedJumps = countAllowedJumps;
+            }
+        }
     }
 }

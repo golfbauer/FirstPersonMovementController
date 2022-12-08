27e8ef01177be9629d5524f4a91d601364e37420
Ansgar Koopmann <koopmannansgar@gmail.com>
2022-12-08 19:57:12 +0100

    PlayerMovementManager started Update, Gravity, Friction

--- Assets/Scripts/Restructure/PlayerMovementManager.cs
+++ Assets/Scripts/Restructure/PlayerMovementManager.cs
@@ -9,9 +9,19 @@ public class PlayerMovementManager : MonoBehaviour
     private KinematicCharacterController kcc;
 
     private Vector3 velocity;
+    private Vector3 movement;
     private float groundedVelocityDeclineRate;
     private float airborneVelocityDeclineRate;
-    private Vector3 gravity;
+
+
+    public Vector3 BaseGravity { get; set; }
+    public float GravityMultiplier { get; set; } = 1;
+    public Vector3 Gravity
+    {
+        get { return BaseGravity * GravityMultiplier; }
+        set { GravityMultiplier = value.magnitude / BaseGravity.magnitude; }
+    }
+    public bool ProjectOnPlane { get; set; } = true;
 
     // List of features that have been added to the controller
     private Dictionary<string, PlayerFeature> features;
@@ -30,7 +40,51 @@ public class PlayerMovementManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ApplyFriction();
+        ApplyGravity();
+
+        List<string> supportedFeatures = new List<string>();
+        foreach (String featureId in activeFeatures)
+        {
+            PlayerFeature feature = features[featureId];
+            feature.CheckAction();
+            supportedFeatures.AddRange(feature.SupportedFeatures);
+
+            if (!feature.IsExecutingAction)
+            {
+                activeFeatures.Remove(featureId);
+            }
+
+        }
+
+
+        movement = velocity * Time.deltaTime;
+        if (ProjectOnPlane)
+        {
+            movement = Vector3.ProjectOnPlane(movement, groundHit.normal);
+        }
+        transform.position = kcc.MovePlayer(movement);
+
+    }
+
+    private void ApplyFriction()
+    {
+        if (IsGrounded())
+        {
+            velocity = velocity.normalized * (velocity.magnitude - groundedVelocityDeclineRate);
+        } 
+        else
+        {
+            velocity = velocity.normalized * (velocity.magnitude - airborneVelocityDeclineRate);
+        }
+    }
+
+    private void ApplyGravity()
+    {
+        if (!IsGrounded()) 
+        {
+            velocity += Gravity;
+        }
     }
 
     /// <summary>
@@ -38,11 +92,40 @@ public class PlayerMovementManager : MonoBehaviour
     /// </summary>
     /// <param name="velocity">Velocity to add. This value should be in units/s and not relative to Time.deltaTime</param>
     /// <param name="maxSpeed">Maximum magnitude of the PlayerMovementManager.velocity vector. This method will add up the the specified value but won't reduce the velocity if it is already higher.</param>
-    public void AddVelocity(Vector3 velocity, float maxSpeed) 
+    public void AddVelocity(Vector3 velocityDelta, float maxSpeed) 
     {
+        float startSpeed = velocity.magnitude;
 
+        velocity += velocityDelta * Time.deltaTime;
+       
+        if (startSpeed > maxSpeed && velocity.magnitude > startSpeed)
+        {
+            velocity = velocity.normalized * startSpeed;
+        } 
+        if (startSpeed < maxSpeed && velocity.magnitude > maxSpeed)
+        {
+            velocity = velocity.normalized * maxSpeed;
+        }
     }
 
+    /// <summary>
+    /// Sets velocity to the specified Value
+    /// </summary>
+    /// <param name="velocity"></param>
+    public void SetVelocity(Vector3 velocity)
+    {
+        this.velocity = velocity;
+    }
+
+    public Vector3 GetVelocity() 
+    { 
+        return this.velocity;
+    }
+
+    /// <summary>
+    /// Determens wether the player is currently staning on ground.
+    /// </summary>
+    /// <returns>True if there is an object <= 0.1f below the player or if player is standing on slope with an angle greater then slope limit; otherwise false</returns>
     public bool IsGrounded()
     {
         return kcc.CheckGrounded(out groundHit);
